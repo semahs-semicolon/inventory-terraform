b3ab62e155785f7b832e7a3983967594881176e1
syeyoung <cyoung06@naver.com>
2024-07-18 19:52:34 +0900

    embedding generator

---
+++ embeddinggenerator.tf
@@ -0,0 +1,16 @@
+// this file is for image embedding generator, for iamge search
+
+module "sagemaker-huggingface" {
+  source  = "philschmid/sagemaker-huggingface/aws"
+  version = "0.9.0"
+  name_prefix          = "image_embedding"
+  pytorch_version      = "1.13.1"
+  transformers_version = "4.26.0"
+  hf_model_id          = "clip-ViT-B-32"
+  hf_task              = "sentence-similarity"
+  serverless_config = {
+    max_concurrency   = 1
+    memory_size_in_mb = 1024
+  }
+  instance_type        = "ml.m5.xlarge"
+}
