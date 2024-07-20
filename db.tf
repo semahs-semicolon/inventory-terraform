ec32a07311106fee31d9b765f48222edf3d01d52
syeyoung <cyoung06@naver.com>
2024-07-20 13:00:43 +0900

    add keypair for db

--- db.tf
+++ db.tf
@@ -6,6 +6,11 @@ resource "aws_ssm_parameter" "database_password" {
   value = "e"
 }
 
+resource "aws_key_pair" "keypair" {
+  key_name = "mykey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/uHBgHAti8a8ecqaC3szieRsPIOKgVfjiC72MJGDCFFOaNuZI36NXYicrTvH4KS2ScUF0R5MktnTHAhC8WhDEN39Qba4TH0FwzkIpAHhFEksHqxfdAe8OocxqF5tcoJCkl2AAF/tGMOw8M7yeaDqvVflGbilJu71sOOfVX+HDYh2jY/3qaKNAUcntS2oD0vFytfWJm9p5Tf9R++kN4nJ2BJLcTOk2W09s4wkzNG0nBC4L51iXiwVALx4HAojxKQkP2YHKtCOdD2G7LglFsIz3IjIoytYpu98E5mtErXOXUf2Yma+MmjzcmlrAJ9ptC/DJLFV4RlFzMV9Pqd0ZGBOZwejawfH7wNFo4q+K9GYNrOVEc2gr62dKReN/fncrRiuu/X6A8WfYdiu2r1aj6Tm/7aCMWg9iL9imfmX40eNEPiRSuA35UGwDwjZvL9afhhbV5L+AnsJHgdIKaja5w7kyAfG2ll5d6+novQ5p7w7/dQThDWzgBcfExjWUr1WVAu8= cyoung06@naver.com"
+}
+
 resource "aws_instance" "database" {
     ami = "ami-09cb0f54fe24c54a6"
     instance_type = "t4g.nano"
@@ -20,5 +25,6 @@ resource "aws_instance" "database" {
     tags = {
         Name = "Database"
     }
-    
+
+    key_name = aws_key_pair.keypair.key_name
 }
