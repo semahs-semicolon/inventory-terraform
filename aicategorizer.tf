c86c5e86e7c7bc9a2f209e2e2a67a726356f4026
syeyoung <cyoung06@naver.com>
2024-07-22 20:00:03 +0900

    categorizer!

--- aicategorizer.tf
+++ aicategorizer.tf
@@ -1,14 +1,9 @@
 # this service categorizes item based on... name. solely name. This is separated from api server because this is long running async job.
 
 
-data "aws_bedrock_foundation_model" "llama8B" {
+data "aws_bedrock_foundation_model" "claude_sonnet" {
   provider = aws.oregon
-  model_id = "meta.llama3-8b-instruct-v1:0"
-}
-
-data "aws_bedrock_foundation_model" "llama70B" {
-  provider = aws.oregon
-  model_id = "meta.llama3-70b-instruct-v1:0"
+  model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
 }
 
 
@@ -31,11 +26,21 @@ data "aws_iam_policy_document" "aicategorizer_perm" {
     effect = "Allow"
 
     resources = [
-        data.aws_bedrock_foundation_model.llama8B.model_arn,
-        data.aws_bedrock_foundation_model.llama70B.model_arn
+        data.aws_bedrock_foundation_model.claude_sonnet.model_arn
     ]
 
     actions = ["bedrock:InvokeModel"]
+  }
+
+
+  statement {
+    effect = "Allow"
+
+    resources = [
+        aws_ssm_parameter.database_password.arn
+    ]
+
+    actions = ["ssm:GetParameter", "kms:Decrypt"]
   }
 }
 
@@ -59,15 +64,32 @@ resource "aws_lambda_function" "aicategorizer" {
   role = aws_iam_role.aicategorizer.arn
 
   
-  filename = "empty.zip"
-  handler = "asdf"
+  filename = data.archive_file.categorizer_code.output_path
+  source_code_hash = data.archive_file.categorizer_code.output_md5
+  handler = "index.handler"
   runtime = "nodejs18.x"
 
-  publish = true
+
+  environment {
+    variables = {
+      "PGHOST": aws_instance.database.public_ip,
+      "PGPORT": "5432",
+      "PGUSER": "inventory",
+      "PGDATABASE": "inventory",
+      "DATABASE_PASSWORD_PARAM_NAME": aws_ssm_parameter.database_password.id,
+      "MODEL_ID": data.aws_bedrock_foundation_model.claude_sonnet.model_id
+    }
+  }
 
 
   # vpc_config {
   #   security_group_ids = [ aws_default_security_group.default_sg.id ]
   #   subnet_ids = [ for k,v in aws_subnet.public_subnets : v.id ]
   # }
+}
+
+data "archive_file" "categorizer_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/categorizer/"
+  output_path = "${path.module}/categorizer.zip"
 }
---
+++ categorizer/index.mjs
@@ -0,0 +1,161 @@
+import { Pool } from 'pg';
+import { BedrockRuntimeClient, ConverseCommand } from "@aws-sdk/client-bedrock-runtime";
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
+
+const ssmClient = new SSMClient();
+const bedrockClient = new BedrockRuntimeClient();
+
+const passwordResp  = await ssmClient.send(new GetParameterCommand({
+    Name: process.env["DATABASE_PASSWORD_PARAM_NAME"],
+    WithDecryption: true
+}));
+
+
+const pool = new Pool({
+    host: process.env["PGHOST"],
+    user: process.env["PGUSER"],
+    port: process.env["PGPORT"],
+    password: passwordResp.Parameter.Value,
+    database: process.env["PGDATABSAE"],
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
+
+
+
+
+const buildCategories = async () => {
+    const pgcategories = await pool.query("SELECT * FROM inventory.categories");
+
+    let categories = [];
+    let categoryMap = {};
+    for (const row of pgcategories.rows) {
+        const cid = row.category_id
+        const name = row.name
+        const pcid = row.parent_category_id;
+        let categoryObj = {
+            id: cid,
+            name: name,
+            parentId: pcid,
+            children: []
+        };
+        categories.push(categoryObj);
+        categoryMap[cid] = categoryObj;
+    }
+
+    for (const category of categories) {
+        if (category.parentId == undefined) continue;
+        category.parent = categoryMap[category.parentId];
+    }
+
+    let mapping = {}
+
+    for (const category of categories) {
+        mapping[category.id] = getCategoryName(category);
+    }
+    return mapping;
+}
+
+const getProduct = async (productId) => {
+    return (await pool.query("SELECT * FROM inventory.products WHERE id = $1", [productId])).rows[0]
+}
+const persistChosen = async (productId, chosenId) => {
+    return await pool.query("UPDATE inventory.products SET categoryAccepted = false, categoryId = $1 WHERE id = $2", [chosenId, productId])
+}
+
+const getCategoryName = (category) => {
+    let name = category.name;
+    category = category.parent;
+    while (category != null) {
+        name = name + " < " + category.name
+        category = category.parent;
+    }
+    return name;
+}
+
+const chooseFinal = async (candidates, item) => {
+    const tool_list = [
+        {
+            "toolSpec": {
+                "name": "set_category",
+                "description": "카테고리를 선택합니다.",
+                "inputSchema": {
+                    "json": {
+                        "type": "object",
+                        "properties": {
+                            "category": {
+                                "type": "string",
+                                "description": "선택한 카테고리",
+                                "enum": candidates.map(a => a.name)
+                            },
+                        },
+                        "required": [
+                            "category"
+                        ]
+                    }
+                }
+            }
+        }
+    ]
+
+    
+    const aiResponse = await bedrockClient.send(new ConverseCommand({
+        modelId: process.env["MODEL_ID"],
+        messages: [
+            {
+                "role": "user",
+                "content": [
+                    { "text": "당신에게 물품의 이름이 주어질 것입니다. 당신의 역할은 주어진 카테고리 목록에서 입력된 물품에 가장 잘 어울리는 카테고리를 하나 골라 그 이유와 함께 출력하는 것 입니다. Chain of Thought 기법을 사용하여 가장 적절한 카테고리를 찾으십시오. 그리고 set_category tool을 사용하여 생각에 기반한 카테고리 json을 생성하십시오."},
+                    { "text": "카테고리 목록: " + candidates.map(a => a.name).join(", ")},
+                    { "text": "### 입력: \n"+item+"\n\n"},
+                    { "text": "### 출력: "}
+                ],
+            }
+        ],
+        inferenceConfig: {
+            maxTokens: 2000,
+            temperature: 0.7
+        },
+        toolConfig: {
+            tools: tool_list,
+            toolChoice: {
+                tool: {
+                    name: "set_category"
+                }
+            }
+        }
+    }));
+    const blocks = aiResponse.output.message.content;
+
+    for (const block of blocks) {
+        if (block.toolUse != null) {
+            const response = block.toolUse.input;
+            return response.category;
+        }
+    }
+    return undefined;
+}
+
+export const handler = async (event) => {
+    const product = await getProduct(event.productId);
+    const categories = await buildCategories();
+
+    // let questions = [];
+    // for (const [id, category] of Object.entries(categories)) {
+    //     // ask model
+    //     if (category == "기타")
+    //         questions.push(Promise.resolve({category: category, id: id, suitable: true}));
+    //     else
+    //         questions.push(askModelForSuitability(category, product.name)
+    //             .then((val) => ({category: category, id: id, suitable: val})));
+    // }
+    // const allAnswers = await Promise.all(questions);
+    // const candidates = allAnswers.filter(a => a.suitable);
+
+    const chosen = await chooseFinal(categories, product.name); // Claude 3 Opus seem quite smart.
+    
+    const chosenCategory = categories.filter(a => a.name == chosen)[0];
+
+    await persistChosen(event.productId, chosenCategory.id);
+}
