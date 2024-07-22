21e8550199507008a98e2bde4e9d9520b25c3cb8
syeyoung <cyoung06@naver.com>
2024-07-22 21:32:45 +0900

    update db provisioning code

--- db.tf
+++ db.tf
@@ -17,6 +17,8 @@ resource "aws_key_pair" "keypair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/uHBgHAti8a8ecqaC3szieRsPIOKgVfjiC72MJGDCFFOaNuZI36NXYicrTvH4KS2ScUF0R5MktnTHAhC8WhDEN39Qba4TH0FwzkIpAHhFEksHqxfdAe8OocxqF5tcoJCkl2AAF/tGMOw8M7yeaDqvVflGbilJu71sOOfVX+HDYh2jY/3qaKNAUcntS2oD0vFytfWJm9p5Tf9R++kN4nJ2BJLcTOk2W09s4wkzNG0nBC4L51iXiwVALx4HAojxKQkP2YHKtCOdD2G7LglFsIz3IjIoytYpu98E5mtErXOXUf2Yma+MmjzcmlrAJ9ptC/DJLFV4RlFzMV9Pqd0ZGBOZwejawfH7wNFo4q+K9GYNrOVEc2gr62dKReN/fncrRiuu/X6A8WfYdiu2r1aj6Tm/7aCMWg9iL9imfmX40eNEPiRSuA35UGwDwjZvL9afhhbV5L+AnsJHgdIKaja5w7kyAfG2ll5d6+novQ5p7w7/dQThDWzgBcfExjWUr1WVAu8= cyoung06@naver.com"
 }
 
+
+// note. it randomly fails due to linux killing the process. When it happens, just delete instance and run it again :D (or -replace aws_instance.database)
 resource "aws_instance" "database" {
     ami = "ami-09cb0f54fe24c54a6"
     instance_type = "t4g.nano"
@@ -77,7 +79,7 @@ resource "aws_instance" "database" {
         cd ..
 
         # I know, I also don't like snap, but this is only way to install aws-cli so please leave it this way. Or you could fix apt-repo but I'm lazy
-        sudo snap install aws-cli --classic
+        snap install aws-cli --classic
         PASSWORD=$(aws ssm get-parameter --name database_password --with-decryption --query "Parameter.Value" --output text)
 
 
@@ -186,6 +188,21 @@ resource "aws_s3_bucket" "dbbackup" {
   bucket = "semicolon-dbdump"
   tags = {
     Name = "semicolon-dbdump"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "dbbackup_lifecycle" {
+  bucket = aws_s3_bucket.dbbackup.id
+  rule {
+    id = "To Glacier"
+    status = "Enabled"
+
+    filter {}
+
+    transition {
+      days = 3
+      storage_class = "GLACIER"
+    }
   }
 }
 
